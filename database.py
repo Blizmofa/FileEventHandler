2282c3ad75845d251ff5246ee979ea1aee086040
Blizmofa <blizmofa@gmail.com>
2023-01-25 16:23:52 +0200

    added custom context manager and high level exceptions handling

--- database.py
+++ database.py
@@ -1,66 +1,64 @@
-"""
-DB class for creating and customize the Consumer needed SQLite tables.
-DB is written with SQL parameterized queries to prevent SQL Injection.
-"""
+import json
 import sqlite3
 from logger import Logger
 
 
-class DB:
+class CustomContextManager:
+    """
+    Custom Context Manager Class to manage DB resources with the 'with' key word.
+    """
+    def __init__(self, name: str):
+        """
+        Initializing DB.
+        """
+        self.name = name
+        self.class_logger = Logger("DB Context Manager")
 
-    def __init__(self):
+    def __enter__(self):
+        """
+        Opens the connection.
+        """
+        self.conn = sqlite3.connect(self.name)
+        self.class_logger.logger.debug(f"Connected to '{self.name}' successfully.")
+        return self.conn.cursor()
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        """
+        Closes the connection.
+        """
+        if self.conn:
+            self.conn.commit()
+            self.conn.close()
+            self.class_logger.logger.debug(f"Saved data and closed the connection to '{self.name}' successfully.")
+
+
+class DB:
+    """
+    ServerDB class for creating and customize the server needed SQLite tables.
+    DB is written with sql parameterized queries to prevent SQL Injection.
+    """
+    def __init__(self, name: str):
         """
         Class Constructor.
-        Initializes the connection and cursor elements to None.
         """
-        self.conn = None
-        self.cursor = None
+        self.name = name
         self.class_logger = Logger('DB')
 
-    def setup_db(self, name):
-        """
-        Setup a new database, initializing the connection and cursor elements accordingly.
-        :param name: For the name of the database to setup.
-        """
-        try:
-            self.conn = sqlite3.connect(name)
-            self.cursor = self.conn.cursor()
-            self.class_logger.logger.info(f"Connected to Database '{name}' successfully.")
-            return True
-        except sqlite3.Error as err:
-            self.class_logger.logger.error(f"Error connecting to database, Error {err}.")
-            return False
-
-    def close_db(self):
-        """
-        Closes database elements and save the data.
-        """
-        try:
-            self.conn.commit()
-            self.class_logger.logger.info(f"Saved all database data successfully.")
-            self.cursor.close()
-            self.class_logger.logger.info(f"Closed '{self.cursor}' successfully.")
-            self.conn.close()
-            self.class_logger.logger.info(f"Closed '{self.conn}' successfully.")
-        except sqlite3.Error as err:
-            print("[!] Unable to close database.")
-            self.class_logger.logger.error(f"Error closing database, Error: {err}")
-
-    def create_table(self, table_name, columns):
+    def create_table(self, table_name: str, columns: str) -> None:
         """
         Creates a table according to a given table name.
         :param table_name: For the table name.
         :param columns: For the table columns.
         """
         try:
-            self.cursor.execute(f"CREATE TABLE IF NOT EXISTS {table_name} ({columns})")
-            self.conn.commit()
-            self.class_logger.logger.info(f"Created Table '{table_name}' successfully.")
+            with CustomContextManager(self.name) as cur:
+                cur.execute(f"CREATE TABLE IF NOT EXISTS {table_name} ({columns})")
+                self.class_logger.logger.debug(f"Created Table '{table_name}' successfully.")
         except sqlite3.Error as err:
-            print(f"[!] Unable to create table '{table_name}'.")
             self.class_logger.logger.error(f"Error creating table {err}.")
+            raise CreateTableError(f"[!] Unable to create table '{table_name}'.")
 
-    def insert_value(self, table_name, table_column, value):
+    def insert_value(self, table_name: str, table_column: str, value: str) -> None:
         """
         Inserting a new value to a given database table.
         :param table_name: For the table to insert values to.
@@ -68,36 +66,41 @@ class DB:
         :param value: For the value to insert.
         """
         try:
-            self.cursor.execute(f"INSERT INTO {table_name} ({table_column}) VALUES(?)", (value,))
-            self.conn.commit()
-            self.class_logger.logger.info(f"Inserted '{value}' to '{table_name}' successfully.")
+            with CustomContextManager(self.name) as cur:
+                cur.execute(f"INSERT INTO {table_name} ({table_column}) VALUES(?)", (value,))
+                self.class_logger.logger.debug(f"Inserted '{value}' to '{table_name}' successfully.")
         except sqlite3.Error as err:
-            print(f"[!] Unable to insert '{value}' to '{table_name}'.")
             self.class_logger.logger.error(f"Error inserting '{value}' to table {err}.")
+            raise InsertError(f"[!] Unable to insert '{value}' to '{table_name}'.")
 
-    def insert_if_not_exists(self, table_name, table_column, value):
+    def insert_if_not_exists(self, table_name: str, table_column: str, value: str) -> bool:
         """
-        Inserting a new value to a given database table only if it doesn't already exists.
+        Inserting a new value to a given database table only if it doesn't already exist.
         :param table_name: For the table to insert values to.
         :param table_column: For the column to insert values to.
         :param value: For the value to insert.
         :return: True if the value has been inserted successfully, False otherwise.
         """
         try:
-            self.cursor.execute(f"SELECT * FROM {table_name} WHERE {table_column}=?", (value,))
-            result = self.cursor.fetchone()
-            if result is None:
-                # Value does not exist and has been inserted successfully
-                self.insert_value(table_name, table_column, value)
-                return True
-            else:
-                self.class_logger.logger.info(f"'{value}' Exists in '{table_name}'")
-                return False
+            with CustomContextManager(self.name) as cur:
+                cur.execute(f"SELECT * FROM {table_name} WHERE {table_column}=?", (value,))
+                result = cur.fetchone()
+                if result is None:
+                    # Value does not exist and has been inserted successfully
+                    try:
+                        self.insert_value(table_name, table_column, value)
+                        return True
+                    except InsertError:
+                        self.class_logger.logger.error(f"[!] Unable to insert '{value}' to '{table_name}'.")
+                        return False
+                else:
+                    self.class_logger.logger.debug(f"'{value}' Exists in '{table_name}'")
+                    return False
         except sqlite3.Error as err:
-            print(f"[!] Unable to insert '{value}' to '{table_name}'.")
             self.class_logger.logger.error(f"Error inserting '{value}' from '{table_name}' {err}.")
 
-    def update_table(self, table_name, column_to_update, value, current_table_column, existing_value):
+    def update_table(self, table_name: str, column_to_update: str, value: str, current_table_column: str,
+                     existing_value: str) -> None:
         """
         Updates an existing table with a given value.
         :param table_name: For the existing table to update.
@@ -107,14 +110,15 @@ class DB:
         :param existing_value: For the existing table value.
         """
         try:
-            self.cursor.execute(f"UPDATE {table_name} SET {column_to_update} = ? WHERE {current_table_column} = ?", (value, existing_value))
-            self.conn.commit()
-            self.class_logger.logger.info(f"Inserted '{value}' to '{column_to_update}' in '{table_name}' successfully.")
-        except (TypeError, sqlite3.Error) as err:
-            print(f"[!] Unable to update '{value}' in '{table_name}'")
+            with CustomContextManager(self.name) as cur:
+                cur.execute(f"UPDATE {table_name} SET {column_to_update} = ? WHERE {current_table_column} = ?",
+                                    (value, existing_value))
+                self.class_logger.logger.debug(f"Inserted '{value}' to '{column_to_update}' in '{table_name}' successfully.")
+        except sqlite3.Error as err:
             self.class_logger.logger.error(f"Error updating table {err}.")
+            raise UpdateError(f"[!] Unable to update '{value}' in '{table_name}'")
 
-    def delete_value(self, table_name, table_column, value_to_delete):
+    def delete_value(self, table_name: str, table_column: str, value_to_delete: str) -> None:
         """
         Deleting a given value from a given table.
         :param table_name: For the table to delete the value from.
@@ -122,14 +126,14 @@ class DB:
         :param value_to_delete: For the value to delete.
         """
         try:
-            self.cursor.execute(f"DELETE FROM {table_name} WHERE {table_column} = ?", (value_to_delete,))
-            self.conn.commit()
-            self.class_logger.logger.info(f"Deleted '{value_to_delete}' from '{table_name}' successfully.")
+            with CustomContextManager(self.name) as cur:
+                cur.execute(f"DELETE FROM {table_name} WHERE {table_column} = ?", (value_to_delete,))
+                self.class_logger.logger.debug(f"Deleted '{value_to_delete}' from '{table_name}' successfully.")
         except sqlite3.Error as err:
-            print(f"[!] Unable to delete '{value_to_delete}' from '{table_name}'.")
             self.class_logger.logger.error(f"Error deleting values from '{table_name}' {err}.")
+            raise DeleteError(f"[!] Unable to delete '{value_to_delete}' from '{table_name}'.")
 
-    def select_value(self, table_name, table_column):
+    def select_value(self, table_name: str, table_column: str) -> None:
         """
         Selects a specific table value and return it.
         :param table_name: For the table to select from.
@@ -137,26 +141,57 @@ class DB:
         :return: The table value after unpacking.
         """
         try:
-            self.cursor.execute(f"SELECT {table_column} FROM {table_name}")
-            value = self.cursor.fetchone()[0]
-            return value
+            with CustomContextManager(self.name) as cur:
+                cur.execute(f"SELECT {table_column} FROM {table_name}")
+                value = cur.fetchone()[0]
+                if value is None:
+                    raise NotFoundError(f"[!] Unable to retrieve value from '{table_name}'")
+                else:
+                    return value
         except sqlite3.Error as err:
-            print(f"[!] Unable to retrieve value from '{table_name}'")
-            self.class_logger.logger.error(f"Error retrieving value from '{table_name}' {err}.")
+            self.class_logger.logger.error(f"Error retrieving value from '{table_name}', Error: {err}.")
 
-    def print_all_database(self, table_name):
+    def export_table_to_json(self, table_name: str) -> None:
         """
-        Prints out to console the entire table in a customized format.
-        :param table_name: For the wanted table to print out.
+        Exports the given table data to a JSON file.
+        :param table_name: For the table to export.
         """
-        counter = 0
-        self.cursor.execute(f"SELECT * FROM {table_name} ")
-        columns = [description[0] for description in self.cursor.description]
-        rows = self.cursor.fetchall()
-        print(f"{'=' * 12} {table_name.upper()} TABLE: {'=' * 12}")
-        for row in rows:
-            counter += 1
-            print(f"File #{counter}: ")
-            for col, val in zip(columns, row):
-                print(f"- {col}: {val}")
-            print("=" * 40)
+        try:
+            with CustomContextManager(self.name) as cur:
+                cur.execute(f"SELECT * FROM {table_name}")
+                data = cur.fetchall()
+                if data is None:
+                    raise NotFoundError(f"[!] Unable to retrieve data from '{table_name}'.")
+                else:
+                    temp = json.dumps(data)
+                    with open(f"{self.name}.json", 'w') as out_file:
+                        out_file.write(temp)
+        except sqlite3.Error as err:
+            self.class_logger.logger.error(f"Error retrieving data from '{table_name}', Error: {err}.")
+
+
+"""
+Custom Exception Classes for raising high-level Exceptions,
+and make DB error handling more informative.
+"""
+
+
+class NotFoundError(Exception):
+    pass
+
+
+class CreateTableError(Exception):
+    pass
+
+
+class InsertError(Exception):
+    pass
+
+
+class UpdateError(Exception):
+    pass
+
+
+class DeleteError(Exception):
+    pass
+
